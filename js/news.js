1e8b047b22deafe2aabfd7f93f53a308d9219567
unknown <afia.nasrin3e@gmail.com>
2022-09-03 17:42:14 +0600

    fynally navbar is compleat

--- js/news.js
+++ js/news.js
@@ -1,1 +1,17 @@
+const loadAllNewsCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/news/categories")
+    .then((res) => res.json())
+    .then((categories) => displayCategoriesName(categories.data.news_category));
+};
 
+const displayCategoriesName = (names) => {
+  for (const name of names) {
+    // console.log(name.category_name);
+    const menu = document.getElementById("menu_all");
+    const li = document.createElement("li");
+    li.innerHTML = `<a>${name.category_name}</a>`;
+    menu.appendChild(li);
+  }
+};
+
+loadAllNewsCategories();
